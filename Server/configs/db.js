502437465d6c89fb63de5f296f50c85d5bec3b2c
RAJESH NAIDU <rn8231019@gmail.com>
2025-11-12 20:40:19 +0530

    some

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -1,11 +1,11 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const connectDb=async()=>{
-//     try {
-//         mongoose.connect(process.env.MONGODB_URI)
-//         mongoose.connection.on('connected',()=>{console.log('Database connected successfully')})
-//     } catch (error) {
-//      console.log('Database connection failed',error)   
-//     }
-// }
-// export default connectDb;
+const connectDb=async()=>{
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/blogs`)
+        mongoose.connection.on('connected',()=>{console.log('Database connected successfully')})
+    } catch (error) {
+     console.log('Database connection failed',error)   
+    }
+}
+export default connectDb;
--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,9 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-// import connectDb from './configs/db.js'
+import connectDb from './configs/db.js'
 import blogRouter from './router/blogRouter.js'
-import mongoose from 'mongoose'
+
 
 const app=express()
 const PORT= process.env.PORT || 8000
@@ -12,13 +12,9 @@ const PORT= process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGODB_URI + '/blog',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(()=>{console.log('mongodb connected')})
 
-//connectDb()
+
+connectDb()
 app.use(blogRouter)
 
 
